946a58b71e65383cb30b1ae35048d222fda0526b
Juan Santillan <juanyut97@hotmail.com>
2025-09-22 18:50:23 -0600

    Form Componentes No Controlados useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,10 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import SentButton from "./components/SentButton";
-import ProductDashboard from "./components/ProductDashboard";
-import ProductList from "./components/ProductList";
+import Form from "./components/Form";
 
 function App() {
-  const [status, setStatus] = useState(true);
-  const toggleStatus = () => setStatus(!status);
-
-  const [sent, setSent] = useState(false);
-  const handleClick = () => setSent(true);
-
-  const [products, setProducts] = useState([
-    {
-      id: Math.random().toString(),
-      name: "Chaneque",
-    },
-  ]);
-
-  // Agregar Productos al final
-  const agregarProductoAlFinal = () => {
-    const newProduct = { id: Math.random().toString(), name: "Barbero Gay" };
-    setProducts([...products, newProduct]);
-  };
-
-  // Agregar Productos al comienzo
-  const agregarProductoAlComienzo = () => {
-    const newProduct = { id: Math.random().toString(), name: "Kikis Cajuelas" };
-    setProducts([newProduct, ...products]);
-  };
-
-  // Modificar un Producto
-  // const modificarProducto = () => {
-  //   const newProducts = products.map((p) =>
-  //     p.id == 1 ? { ...p, name: "Windows" } : p
-  //   );
-  //   setProducts(newProducts);
-  // };
-
-  // Eliminar último Producto
-  const eliminarUltimoProducto = () => {
-    const newProducts = products.slice(0, -1);
-    setProducts(newProducts);
-  };
-
-  // Limpiar productos
-  const limpiarProductos = () => {
-    setProducts([]);
-  };
-
   return (
-    <>
-      {console.log(products)}
-      {/* <Alert status={status} onClick={toggleStatus}>
-        Alerta!
-      </Alert>
-
-      <SentButton sent={sent} onClick={handleClick}>
-        Enviar
-      </SentButton> */}
-      <div>
-        <Button onClick={agregarProductoAlFinal}>
-          Agregar Chaneque al Final
-        </Button>
-        <hr />
-        <Button onClick={agregarProductoAlComienzo}>
-          Agregar Chaneque al Comienzo
-        </Button>
-        <hr />
-        <Button onClick={limpiarProductos}>Limpiar Chaneques</Button>
-        <hr />
-        <Button onClick={eliminarUltimoProducto}>
-          Eliminar último Chaneque
-        </Button>
-        <hr />
-        <ProductDashboard amount={products.length} />
-        <ProductList products={products} />
-      </div>
-    </>
+    <div className="container">
+      <Form />
+    </div>
   );
 }
 
---
+++ src/components/Form/index.tsx
@@ -0,0 +1,44 @@
+import React, { FormEvent, useRef } from "react";
+
+type Props = {};
+
+function Form({}: Props) {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const lastnameRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    const user = {
+      name: nameRef.current?.value,
+      lastname: lastnameRef.current?.value,
+    };
+
+    console.log(user);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Nombre
+        </label>
+        <input ref={nameRef} type="text" id="name" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="lastname" className="form-label">
+          Apellido
+        </label>
+        <input
+          ref={lastnameRef}
+          type="text"
+          id="lastname"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary">Enviar</button>
+    </form>
+  );
+}
+
+export default Form;
