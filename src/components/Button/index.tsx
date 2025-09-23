d748dc735eeb0b70e54eabfa1b96200dc5dc0169
Juan Santillan <juanyut97@hotmail.com>
2025-09-22 18:08:12 -0600

    Gestion del estado y ejemplos

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,17 +3,19 @@ import { FaRegThumbsUp } from "react-icons/fa";
 import styled from "styled-components";
 
 type BtnProps = {
-  isLoading: boolean;
+  isLoading?: boolean;
 };
 
 const Btn = styled.button<BtnProps>`
-  background: ${(props) => (props.isLoading ? "gray" : "red")};
-  padding: 25px 30px;
+  background: ${(props) => (props.isLoading ? "gray" : "#fdfd96")};
+  padding: 15px 30px;
+  border-radius: 8px;
+  border: solid 1px black;
 `;
 
 type Props = {
   children: ReactNode;
-  isLoading: boolean;
+  isLoading?: boolean;
   onClick: () => void;
 };
 
@@ -21,7 +23,7 @@ function Button({ children, isLoading, onClick }: Props) {
   return (
     <Btn onClick={onClick} disabled={isLoading} isLoading={isLoading}>
       {isLoading ? "Cargando..." : children}
-      <FaRegThumbsUp color="white" size={20} />
+      {/* <FaRegThumbsUp color="white" size={20} /> */}
     </Btn>
   );
 }
